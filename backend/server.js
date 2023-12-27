cd265e2d60c8e9fa69705f6584d2dca0c6fc0f82
Анастасия Колмакова <nastya.miss.7@gmail.com>
2023-12-27 05:18:10 +0400

    fixing port probl

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const { Server } = require('socket.io');
 const cors = require('cors');
 const { router, getRooms } = require('./routes/rooms');
@@ -8,6 +9,12 @@ const handleRSPSocket = require('./rspSocket');
 const TTTRoom = require('./classes/TTTRoom');
 
 const app = express();
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
